6f6a5da32c736daf36a7120c16a789d2ef47a4c8
s.andreyuk <s.andreyuk@crpt.ru>
2022-11-06 23:44:31 +0300

    - fix checkstyle errors (+3)

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Data
-class BaseSchema {
+public class BaseSchema {
     private Object dataToCheck;
     private List<String> checkList = new ArrayList<>();
     private boolean schemaIsValid;
