2ea27b19c91fe0d89fad0c15b4e9ddbe9a921b33
s.andreyuk <s.andreyuk@crpt.ru>
2022-11-10 10:57:24 +0300

    - add javadoc to baseschema class

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -5,22 +5,45 @@ import lombok.Data;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Класс базовой схемы со свойствами <b>dataToCheck</b>, <b>checkList</b> и <b>schemaIsValid</b>.
+ * @author s.andreyuk or Aphie
+ * @version 1.0
+ */
 @Data
 public class BaseSchema {
+    /** Поле данных для проверки */
     private Object dataToCheck;
+
+    /** Список проверок */
     private List<String> checkList = new ArrayList<>();
+
+    /** Признак валидности системы */
     private boolean schemaIsValid;
 
+    /**
+     * Метод - базовая проверка на обязательность
+     * @return - возвращает базовую схему в неизменном виде
+     */
     public BaseSchema toCheckIfRequired() {
         return this;
     }
 
+    /**
+     * Метод - сеттер добавления проверок в список проверок
+     * @param value - добавляемая проверка
+     */
     public void setCheckList(String value) {
         if (!checkList.contains(value)) {
             checkList.add(value);
         }
     }
 
+    /**
+     * Метод - базовая проверка валидности введенных данных
+     * @param input - проверяемые данные
+     * @return - признак валидности данных в соответствии со списком проверок
+     */
     public boolean isValid(Object input) {
         this.schemaIsValid = true;
         this.dataToCheck = input;
